315eab827b6c13d9127ea5070b28e9874a0997d8
inti0221 <inti0221@INTI-0221>
2016-07-12 17:25:56 +0200

    commit client

--- ProxyBanque/src/main/java/fr/adaming/model/Client.java
+++ ProxyBanque/src/main/java/fr/adaming/model/Client.java
@@ -1,5 +1,113 @@
 package fr.adaming.model;
 
-public class Client {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name="clientBean")
+@Table(name="clients")
+public class Client implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	////
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_client")
+	private int id;
+	@Column(name="nom")
+	private String nom;
+	@Column(name="prenom")
+	private String prenom;
+	@Column(name="adresse")
+	private String adresse;
+	@Column(name="CP")
+	private String codePostal;
+	@Column(name="ville")
+	private String ville;
+	@Column(name="tel")
+	private String telephone;
+	
+	////
+	public Client() {
+		super();
+	}
+	public Client(String nom, String prenom, String adresse, String codePostal,
+			String ville, String telephone) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.telephone = telephone;
+	}
+	public Client(int id, String nom, String prenom, String adresse,
+			String codePostal, String ville, String telephone) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.telephone = telephone;
+	}
+	
+	////
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getCodePostal() {
+		return codePostal;
+	}
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	
+	////
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", nom=" + nom + ", prenom=" + prenom
+				+ ", adresse=" + adresse + ", codePostal=" + codePostal
+				+ ", ville=" + ville + ", telephone=" + telephone + "]";
+	}
 }
