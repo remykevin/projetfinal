8acfc58232c62d8ddba59da25fb3ccd15548e8ef
inti0293 <inti0293@INTI-0293>
2016-07-13 11:45:00 +0200

    commit application context

---
+++ ProxyBanque/src/main/java/fr/adaming/dao/GestionClientDaoImpl.java
@@ -0,0 +1,31 @@
+package fr.adaming.dao;
+
+import fr.adaming.model.Client;
+
+public class GestionClientDaoImpl implements IGestionClientDao{
+
+	@Override
+	public void ajouterClient(Client client) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void supprimmerClient(Client client) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modifierClient(Client client) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Client afficherClientParId(int idClient) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- ProxyBanque/src/main/java/fr/adaming/dao/IGestionClientDao.java
+++ ProxyBanque/src/main/java/fr/adaming/dao/IGestionClientDao.java
@@ -5,4 +5,6 @@ import fr.adaming.model.Client;
 public interface IGestionClientDao {
 public void ajouterClient (Client client);
 public void supprimmerClient (Client client);
+public void modifierClient (Client client);
+public Client afficherClientParId(int idClient);
 }
