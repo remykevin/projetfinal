4b16585e60f6e631a9b8d6557f4528354797b7a2
inti0293 <inti0293@INTI-0293>
2016-07-13 13:20:25 +0200

    commit managed bean gestion

--- ProxyBanque/src/main/java/fr/adaming/dao/GestionClientDaoImpl.java
+++ ProxyBanque/src/main/java/fr/adaming/dao/GestionClientDaoImpl.java
@@ -5,12 +5,25 @@ import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.adaming.model.Client;
-
+@Repository
+@Transactional
 public class GestionClientDaoImpl implements IGestionClientDao{
 	@Autowired 
 	private SessionFactory sessionFactory;
+	
+	
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
 	@Override
 	public void ajouterClient(Client client) {
 		Session session = sessionFactory.openSession();
