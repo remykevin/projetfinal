cb442aabd8184885da56acf1a0e4bedbf25539d3
inti0293 <inti0293@INTI-0293>
2016-07-13 12:15:12 +0200

    commit gestion client dao

--- ProxyBanque/src/main/java/fr/adaming/dao/GestionClientDaoImpl.java
+++ ProxyBanque/src/main/java/fr/adaming/dao/GestionClientDaoImpl.java
@@ -1,31 +1,59 @@
 package fr.adaming.dao;
 
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import fr.adaming.model.Client;
 
 public class GestionClientDaoImpl implements IGestionClientDao{
-
+	@Autowired 
+	private SessionFactory sessionFactory;
 	@Override
 	public void ajouterClient(Client client) {
-		// TODO Auto-generated method stub
+		Session session = sessionFactory.openSession();
+		session.save(client);
+		session.close();
 		
 	}
 
 	@Override
 	public void supprimmerClient(Client client) {
-		// TODO Auto-generated method stub
+		Session session = sessionFactory.openSession();
+		String hqlReq="delete clientBean  where id =:id1";
+		Query query=session.createQuery(hqlReq);
+		query.setParameter("id1", client.getId());
+		query.executeUpdate();
 		
 	}
 
 	@Override
 	public void modifierClient(Client client) {
-		// TODO Auto-generated method stub
+		Session session = sessionFactory.openSession();
+		String hqlReq="update clientBean set nom=:nom1, prenom= :prenom1 , codePostal=:cp1,adresse=:adresse1,ville=:ville1,"
+				+ " telephone=:telephone1 "+ " where id =:id1";
+		Query query=session.createQuery(hqlReq);
+		query.setParameter("nom1", client.getNom());
+		query.setParameter("prenom1", client.getPrenom());
+		query.setParameter("cp1", client.getCodePostal());
+		query.setParameter("adresse1", client.getAdresse());
+		query.setParameter("ville1", client.getVille());
+		query.setParameter("telephone1", client.getTelephone());	
+		query.setParameter("id1", client.getId());
+		query.executeUpdate();
 		
 	}
 
 	@Override
 	public Client afficherClientParId(int idClient) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = sessionFactory.openSession();
+		String sqlReq="select * from clients  where id =:id1 ";
+		SQLQuery query=session.createSQLQuery(sqlReq);
+		query.addEntity(Client.class);
+		query.setParameter("id1", idClient);
+		return (Client) query.uniqueResult();
 	}
 
 }
