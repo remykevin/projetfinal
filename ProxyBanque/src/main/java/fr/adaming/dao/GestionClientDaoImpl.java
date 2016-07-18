0f4bc486ca8c482da94c69ff8ded412c8fc32bcd
inti0221 <inti0221@INTI-0221>
2016-07-18 11:15:49 +0200

    commit nomane

--- ProxyBanque/src/main/java/fr/adaming/dao/GestionClientDaoImpl.java
+++ ProxyBanque/src/main/java/fr/adaming/dao/GestionClientDaoImpl.java
@@ -9,33 +9,34 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import fr.adaming.model.Client;
-@Repository
 
+@Repository
+@Transactional
 public class GestionClientDaoImpl implements IGestionClientDao{
+	
 	@Autowired 
 	private SessionFactory sessionFactory;
 	
-	
+	////
 	public SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
-
 	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
 
+	////
 	@Override
-	@Transactional
 	public void ajouterClient(Client client) {
-		Session session = sessionFactory.openSession();
-		session.save(client);
-		session.close();
 		
+		Session session = sessionFactory.getCurrentSession();
+		session.save(client);		
 	}
 
 	@Override
 	public void supprimmerClient(Client client) {
-		Session session = sessionFactory.openSession();
+		
+		Session session = sessionFactory.getCurrentSession();
 		String hqlReq="delete clientBean  where id =:id1";
 		Query query=session.createQuery(hqlReq);
 		query.setParameter("id1", client.getId());
@@ -45,7 +46,8 @@ public class GestionClientDaoImpl implements IGestionClientDao{
 
 	@Override
 	public void modifierClient(Client client) {
-		Session session = sessionFactory.openSession();
+		
+		Session session = sessionFactory.getCurrentSession();
 		String hqlReq="update clientBean set nom=:nom1, prenom= :prenom1 , codePostal=:cp1,adresse=:adresse1,ville=:ville1,"
 				+ " telephone=:telephone1 "+ " where id =:id1";
 		Query query=session.createQuery(hqlReq);
@@ -62,7 +64,8 @@ public class GestionClientDaoImpl implements IGestionClientDao{
 
 	@Override
 	public Client afficherClientParId(int idClient) {
-		Session session = sessionFactory.openSession();
+		
+		Session session = sessionFactory.getCurrentSession();
 		String sqlReq="select * from clients  where id =:id1 ";
 		SQLQuery query=session.createSQLQuery(sqlReq);
 		query.addEntity(Client.class);
